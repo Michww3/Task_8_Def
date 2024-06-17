e63e12c176a366695103d250f017387b574f59cc
User <User@W11PC>
2024-06-17 09:30:12 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml.Serialization;
+
+class Programm
+{
+    static void Main (string[] args)
+    {
+        Dog dog = new Dog("Apple",4);
+        XmlSerializer xmlSerializer  = new XmlSerializer(typeof(Dog));
+        using (StreamWriter  fileStream = new StreamWriter("User.xml"))
+        {
+            xmlSerializer.Serialize(fileStream, dog);
+        }
+    }
+}
+[Serializable]
+public class Dog
+{
+    public string name;
+    public int age;
+
+    public Dog(string name , int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+    public Dog()
+    {
+
+    }
+}
